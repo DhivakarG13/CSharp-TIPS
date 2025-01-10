db4c64d40cf1055e76ea155436185b386ea4def5
Dhivakar Gopi <dhivakar.gopi@solitontech.com>
2025-01-10 16:10:53 +0530

    feat : General Exception Handling

--- Assignment_1/ContactManager.cs
+++ Assignment_1/ContactManager.cs
@@ -7,36 +7,46 @@ List<ContactInfo> Recents = new List<ContactInfo>();
 while (true)
 {
     UserInteraction.PrintMenu();
-    int UserChoice = int.Parse(Console.ReadLine());
-    if (UserChoice == 1) // create contact
+    try
     {
-        CallLog.Add(UserInteraction.GetDetails());
+        int UserChoice = int.Parse(Console.ReadLine());
+        if (UserChoice == 1) // create contact
+        {
+            CallLog.Add(UserInteraction.GetDetails());
+        }
+        else if (UserChoice == 2)// Search contact
+        {
+            Console.Write("Enter Name          :");
+            string? name = Console.ReadLine();
+            Recents.AddRange(UserInteraction.FindContact(name, CallLog));
+        }
+        else if (UserChoice == 3)// View Dierectory
+        {
+            UserInteraction.OperateInContact(0, CallLog);
+        }
+        else if (UserChoice == 4)//Recent Searches
+        {
+            UserInteraction.OperateInContact(0, Recents);
+        }
+        else if (UserChoice == 5) //Favourites
+        {
+            UserInteraction.OperateInContact(1, CallLog);
+        }
+        else if (UserChoice == 6)
+        {
+            break;
+        }
+        else
+        {
+            Console.WriteLine("Invalid Input, Enter a valid Option");
+        }
+        Console.WriteLine("-------------------------------------------------");
     }
-    else if (UserChoice == 2)// Search contact
+
+
+    catch
     {
-        Console.Write("Enter Name          :");
-        string? name = Console.ReadLine();
-        Recents.AddRange(UserInteraction.FindContact(name, CallLog));
+        Console.WriteLine("Invalid Input, Enter a valid Option/n");
     }
-    else if (UserChoice == 3)// View Dierectory
-    {
-        UserInteraction.OperateInContact(0, CallLog);
-    }
-    else if (UserChoice == 4)//Recent Searches
-    {
-        UserInteraction.OperateInContact(0, Recents);
-    }
-    else if (UserChoice == 5) //Favourites
-    {
-        UserInteraction.OperateInContact(1, CallLog);
-    }
-    else if (UserChoice == 6)
-    {
-        break;
-    }
-    else
-    {
-        Console.WriteLine("Invalid Input, Enter a valid Option");
-    }
-    Console.WriteLine("-------------------------------------------------");
+
 }
