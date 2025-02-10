b83337dd3312808434d33a6381736811a7c05c76
Dhivakar Gopi <dhivakar.gopi@solitontech.com>
2025-02-10 19:05:47 +0530

    Assignment_8_ Task1,2,3

---
+++ Task3_CustomException/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace Task3_CustomException
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a number :");
+            int numericalInput = default;
+            try
+            {
+                while (int.TryParse(Console.ReadLine(), out numericalInput) == false)
+                {
+
+                    throw new InvalidUserInputException("Your value is Invalid It's Assigned to 0");
+                    throw new InvalidUserInputException();
+                }
+            }
+            catch (InvalidUserInputException ex)
+            {
+                Console.WriteLine(ex.Message);
+                numericalInput = 0;
+            }
+            catch (Exception ex)
+            { 
+                Console.WriteLine(ex.Message);
+                numericalInput = 0;
+            }
+            finally
+            {
+                Console.WriteLine($"Your Input : {numericalInput}");
+                Console.ReadKey();
+            }
+
+        }
+    }
+}
