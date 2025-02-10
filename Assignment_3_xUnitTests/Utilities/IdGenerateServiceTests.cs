499550e921a1a93ee32d14b8b2b11b66c43ae4e8
Dhivakar Gopi <dhivakar.gopi@solitontech.com>
2025-02-10 22:10:18 +0530

    Added tests for Product name Validate

--- Assignment_3_xUnitTests/Utilities/IdGenerateServiceTests.cs
+++ Assignment_3_xUnitTests/Utilities/IdGenerateServiceTests.cs
@@ -3,10 +3,34 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Inventory.Helpers;
 
 namespace Assignment_3_xUnitTests.Utilities
 {
-    internal class IdGenerateServiceTests
+    public class IdGenerateServiceTests
     {
+        [Theory]
+        [InlineData(2004)]
+        [InlineData(2005)]
+        [InlineData(2006)]
+
+        public void GivenProducts_WhenProductIdGenerator_ReturnsUniqueId(int oldId)
+        {
+            List<Product> testProducts = GenerateProducts(3);
+
+            int actualId = IdGenerateService.ProductIdGenerator(testProducts);
+
+            Assert.NotEqual(oldId, actualId);
+            Assert.InRange(actualId, 1000, 10000);
+        }
+        private List<Product> GenerateProducts(int countOfProducts)
+        {
+            List<Product> testProducts = new List<Product>();
+            for (int index = 0; index < countOfProducts; index++)
+            {
+                testProducts.AddRange(new Product[] { new Product($"Product{index}", 20 + index, 200 + index, 2004 + index, DateOnly.MinValue) });
+            }
+            return testProducts;
+        }
     }
 }
