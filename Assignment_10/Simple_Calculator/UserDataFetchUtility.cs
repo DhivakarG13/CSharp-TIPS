c2ab0dc49688bd8282156756e6abd9550d6a9895
Dhivakar Gopi <dhivakar.gopi@solitontech.com>
2025-02-20 15:08:26 +0530

    feat : simple calculator

---
+++ Assignment_10/Simple_Calculator/UserDataFetchUtility.cs
@@ -0,0 +1,44 @@
+﻿namespace Simple_Calculator
+{
+    public static class UserDataFetchUtility
+    {
+        public static int GetNumberToCalculate(string dataToFetch)
+        {
+            string? userInput = null;
+            int parsedInput = default;
+            bool isValidNumber = false;
+            while (!isValidNumber)
+            {
+                Console.Write($"Enter {dataToFetch} : ");
+                userInput = Console.ReadLine();
+                isValidNumber = int.TryParse(userInput, out parsedInput);
+                if (!isValidNumber)
+                {
+                    Console.WriteLine("Numerical value expected");
+                }
+            }
+            return parsedInput;
+        }
+        public static int GetDialogChoice(int totalOptions)
+        {
+            bool isValidChoice = false;
+            int choice = GetNumberToCalculate("Choice ");
+
+            while (!isValidChoice)
+            {
+                isValidChoice = UserDataValidate.ValidateChoice(choice, totalOptions);
+                if (!isValidChoice)
+                {
+                    Console.WriteLine("Enter a valid Input try Again");
+                }
+                else
+                {
+                    break;
+                }
+                choice = GetNumberToCalculate("Choice ");
+            }
+
+            return choice;
+        }
+    }
+}
---
+++ Assignment_10/Simple_Calculator/UserDataValidate.cs
@@ -0,0 +1,14 @@
+﻿namespace Simple_Calculator
+{
+    public static class UserDataValidate
+    {
+        public static bool ValidateChoice(int choice, int totalOptions)
+        {
+            if (choice > 0 && choice <= totalOptions)
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
