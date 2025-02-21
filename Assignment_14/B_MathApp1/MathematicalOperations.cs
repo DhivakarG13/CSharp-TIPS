c9129c53ae4336708dbecb2d934806ca2520d6aa
Dhivakar Gopi <dhivakar.gopi@solitontech.com>
2025-02-21 18:07:24 +0530

    feat : practical exercise

---
+++ Assignment_14/B_MathApp1/MathematicalOperations.cs
@@ -0,0 +1,20 @@
+﻿namespace B_MathApp
+{
+    public static class MathematicalOperations
+    {
+        public static int Add(int x, int y) => x + y;
+
+        public static int Subtract(int x, int y) => x - y;
+
+        public static int Multiply(int x, int y) => x * y;
+
+        public static int Divide(int x, int y)
+        {
+            if (y == 0)
+            { 
+                throw new DivideByZeroException("Second Number cannot be zero");
+            }
+            return x / y;
+        }
+    }
+}
