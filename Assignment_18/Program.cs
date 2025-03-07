8c7eeba1e0c69e68aec1880b4bc0a7af5898d398
Dhivakar Gopi <dhivakar.gopi@solitontech.com>
2025-03-07 20:57:36 +0530

    feat: task 1,2,3,4,5,6,7

---
+++ Assignment_18/Program.cs
@@ -0,0 +1,31 @@
+﻿using System.Net;
+
+namespace Assignment_18
+{
+    public class Program
+    {
+        static async Task Main(string[] args)
+        {
+            WebDataFetchUtility webDataFetchUtility = new WebDataFetchUtility();
+            try
+            {
+                string DataFromWeb = await webDataFetchUtility.FetchDataFromLink("https://en.wikipedia.org/wiki/C_Sharp_(programming_language)");
+                Console.WriteLine(DataFromWeb);
+            }
+            catch (HttpRequestException ex) when (ex.StatusCode == HttpStatusCode.NotFound)
+            {
+                Console.WriteLine("Resource not found.");
+            }
+            catch (HttpRequestException ex)
+            {
+                Console.WriteLine($"Request error: {ex.Message}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"An UnExpected error occurred: {ex.Message}");
+            }
+            Console.WriteLine("Press Any Key To Close");
+            Console.ReadKey();
+        }
+    }
+}
